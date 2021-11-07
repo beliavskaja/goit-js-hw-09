59330f6973eb5cc375d80c23c664cde227a0d98a
Ekaterina Beliavskaja <beliavskaja.ekaterina@gmail.com>
2021-11-07 12:28:49 +0100

    part 1 ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let interval = undefined;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", () => {
+    interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    stopButton.removeAttribute('disabled');
+startButton.setAttribute('disabled', true);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    startButton.removeAttribute('disabled');
+stopButton.setAttribute('disabled', true);
+});
